4e8b5da736cf25fe181f61ee1bf4a36160e1f54d
Sibongakonke10 <nqobanizibane2004@gmail.com>
2024-12-21 16:08:29 +0200

    Add files via upload
    ISP Service Provider Application

---
+++ Internet Service Provider Contractual App/Internet Service Provider Contractual App/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿
+namespace Internet_Service_Provider_Contractual_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.CellTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TalkBox = new System.Windows.Forms.TextBox();
+            this.DataBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AirtimeBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MixMaxButton = new System.Windows.Forms.RadioButton();
+            this.DDButton = new System.Windows.Forms.RadioButton();
+            this.ChatterButton = new System.Windows.Forms.RadioButton();
+            this.payuButton = new System.Windows.Forms.RadioButton();
+            this.TandCBox = new System.Windows.Forms.CheckBox();
+            this.OKbutton = new System.Windows.Forms.Button();
+            this.CLEARbutton = new System.Windows.Forms.Button();
+            this.EXITbutton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.CellTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.NameBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(329, 142);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customer Details";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(103, 59);
+            this.maskedTextBox1.Mask = "9990000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            // 
+            // CellTextBox
+            // 
+            this.CellTextBox.Location = new System.Drawing.Point(103, 87);
+            this.CellTextBox.Mask = "999 000 0000";
+            this.CellTextBox.Name = "CellTextBox";
+            this.CellTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CellTextBox.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.CellTextBox, "Phone Number Associated With Contract");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Requested Cell";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID Number";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(103, 33);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(151, 20);
+            this.NameBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.TalkBox);
+            this.groupBox2.Controls.Add(this.DataBox);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.AirtimeBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.MixMaxButton);
+            this.groupBox2.Controls.Add(this.DDButton);
+            this.groupBox2.Controls.Add(this.ChatterButton);
+            this.groupBox2.Controls.Add(this.payuButton);
+            this.groupBox2.Location = new System.Drawing.Point(13, 162);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(329, 208);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Contract Details";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(260, 174);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Minutes";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(260, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "GB";
+            // 
+            // TalkBox
+            // 
+            this.TalkBox.Location = new System.Drawing.Point(103, 167);
+            this.TalkBox.Name = "TalkBox";
+            this.TalkBox.Size = new System.Drawing.Size(151, 20);
+            this.TalkBox.TabIndex = 9;
+            // 
+            // DataBox
+            // 
+            this.DataBox.Location = new System.Drawing.Point(103, 141);
+            this.DataBox.Name = "DataBox";
+            this.DataBox.Size = new System.Drawing.Size(151, 20);
+            this.DataBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Talktime";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Data";
+            // 
+            // AirtimeBox
+            // 
+            this.AirtimeBox.Location = new System.Drawing.Point(103, 109);
+            this.AirtimeBox.Name = "AirtimeBox";
+            this.AirtimeBox.Size = new System.Drawing.Size(151, 20);
+            this.AirtimeBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Airtime";
+            // 
+            // MixMaxButton
+            // 
+            this.MixMaxButton.AutoSize = true;
+            this.MixMaxButton.Location = new System.Drawing.Point(169, 65);
+            this.MixMaxButton.Name = "MixMaxButton";
+            this.MixMaxButton.Size = new System.Drawing.Size(93, 17);
+            this.MixMaxButton.TabIndex = 3;
+            this.MixMaxButton.TabStop = true;
+            this.MixMaxButton.Text = "Mix Marksman";
+            this.MixMaxButton.UseVisualStyleBackColor = true;
+            this.MixMaxButton.CheckedChanged += new System.EventHandler(this.MixMaxButton_CheckedChanged);
+            // 
+            // DDButton
+            // 
+            this.DDButton.AutoSize = true;
+            this.DDButton.Location = new System.Drawing.Point(169, 31);
+            this.DDButton.Name = "DDButton";
+            this.DDButton.Size = new System.Drawing.Size(79, 17);
+            this.DDButton.TabIndex = 2;
+            this.DDButton.TabStop = true;
+            this.DDButton.Text = "Data Driver";
+            this.DDButton.UseVisualStyleBackColor = true;
+            this.DDButton.CheckedChanged += new System.EventHandler(this.DDButton_CheckedChanged);
+            // 
+            // ChatterButton
+            // 
+            this.ChatterButton.AutoSize = true;
+            this.ChatterButton.Location = new System.Drawing.Point(10, 65);
+            this.ChatterButton.Name = "ChatterButton";
+            this.ChatterButton.Size = new System.Drawing.Size(93, 17);
+            this.ChatterButton.TabIndex = 1;
+            this.ChatterButton.TabStop = true;
+            this.ChatterButton.Text = "Chatter Button";
+            this.ChatterButton.UseVisualStyleBackColor = true;
+            this.ChatterButton.CheckedChanged += new System.EventHandler(this.ChatterButton_CheckedChanged);
+            // 
+            // payuButton
+            // 
+            this.payuButton.AutoSize = true;
+            this.payuButton.Location = new System.Drawing.Point(10, 31);
+            this.payuButton.Name = "payuButton";
+            this.payuButton.Size = new System.Drawing.Size(102, 17);
+            this.payuButton.TabIndex = 0;
+            this.payuButton.TabStop = true;
+            this.payuButton.Text = "Pay As You Use";
+            this.payuButton.UseVisualStyleBackColor = true;
+            this.payuButton.CheckedChanged += new System.EventHandler(this.payuButton_CheckedChanged);
+            // 
+            // TandCBox
+            // 
+            this.TandCBox.AutoSize = true;
+            this.TandCBox.Location = new System.Drawing.Point(392, 331);
+            this.TandCBox.Name = "TandCBox";
+            this.TandCBox.Size = new System.Drawing.Size(166, 17);
+            this.TandCBox.TabIndex = 2;
+            this.TandCBox.Text = "Accept Terms And Conditions";
+            this.toolTip2.SetToolTip(this.TandCBox, "You Must Accept Terms And Conditions");
+            this.TandCBox.UseVisualStyleBackColor = true;
+            this.TandCBox.CheckedChanged += new System.EventHandler(this.TandCBox_CheckedChanged);
+            // 
+            // OKbutton
+            // 
+            this.OKbutton.Enabled = false;
+            this.OKbutton.Location = new System.Drawing.Point(392, 346);
+            this.OKbutton.Name = "OKbutton";
+            this.OKbutton.Size = new System.Drawing.Size(75, 23);
+            this.OKbutton.TabIndex = 3;
+            this.OKbutton.Text = "OK";
+            this.OKbutton.UseVisualStyleBackColor = true;
+            this.OKbutton.Click += new System.EventHandler(this.OKbutton_Click);
+            // 
+            // CLEARbutton
+            // 
+            this.CLEARbutton.Location = new System.Drawing.Point(501, 346);
+            this.CLEARbutton.Name = "CLEARbutton";
+            this.CLEARbutton.Size = new System.Drawing.Size(75, 23);
+            this.CLEARbutton.TabIndex = 4;
+            this.CLEARbutton.Text = "CLEAR";
+            this.CLEARbutton.UseVisualStyleBackColor = true;
+            this.CLEARbutton.Click += new System.EventHandler(this.CLEARbutton_Click);
+            // 
+            // EXITbutton
+            // 
+            this.EXITbutton.Location = new System.Drawing.Point(609, 346);
+            this.EXITbutton.Name = "EXITbutton";
+            this.EXITbutton.Size = new System.Drawing.Size(75, 23);
+            this.EXITbutton.TabIndex = 5;
+            this.EXITbutton.Text = "EXIT";
+            this.toolTip3.SetToolTip(this.EXITbutton, "EXITS THE APPLICATION");
+            this.EXITbutton.UseVisualStyleBackColor = true;
+            this.EXITbutton.Click += new System.EventHandler(this.EXITbutton_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Tag = "Phone Number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.EXITbutton);
+            this.Controls.Add(this.CLEARbutton);
+            this.Controls.Add(this.OKbutton);
+            this.Controls.Add(this.TandCBox);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Internet Service Provider Contractural App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox CellTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TalkBox;
+        private System.Windows.Forms.TextBox DataBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox AirtimeBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton MixMaxButton;
+        private System.Windows.Forms.RadioButton DDButton;
+        private System.Windows.Forms.RadioButton ChatterButton;
+        private System.Windows.Forms.RadioButton payuButton;
+        private System.Windows.Forms.CheckBox TandCBox;
+        private System.Windows.Forms.Button OKbutton;
+        private System.Windows.Forms.Button CLEARbutton;
+        private System.Windows.Forms.Button EXITbutton;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+    }
+}
+
---
+++ Internet Service Provider Contractual App/Internet Service Provider Contractual App/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Internet_Service_Provider_Contractual_App
+{
+    public partial class Form1 : Form
+    {
+        decimal totalcharge;
+        decimal servicecharge = 20;
+        decimal airtime;
+        decimal data;
+        decimal talktime;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void payuButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(payuButton.Checked==true)
+            {
+                TalkBox.Enabled = false;
+                TalkBox.Text = "0";
+                DataBox.Enabled = false;
+                DataBox.Text = "0";
+            }
+        }
+
+        private void ChatterButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(ChatterButton.Checked==true)
+            {
+                AirtimeBox.Enabled = false;
+                AirtimeBox.Text = "0";
+                DataBox.Enabled = false;
+                DataBox.Text = "0";
+            }
+        }
+
+        private void DDButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(DDButton.Checked==true)
+            {
+                TalkBox.Enabled = false;
+                TalkBox.Text = "0";
+                AirtimeBox.Enabled = false;
+                AirtimeBox.Text = "0";
+            }
+        }
+
+        private void MixMaxButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(MixMaxButton.Checked==true)
+            {
+                AirtimeBox.Enabled = false;
+                AirtimeBox.Text = "0";
+            }
+        }
+
+        private void TandCBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(TandCBox.Checked==true)
+            {
+                OKbutton.Enabled = true;
+            }
+            else
+            {
+                OKbutton.Enabled = false;
+            }
+        }
+
+        private void CLEARbutton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are You sure you want to clear all the contents", "Caution", MessageBoxButtons.YesNo);
+            if(result==DialogResult.Yes)
+            {
+                TalkBox.Clear();
+                DataBox.Clear();
+                AirtimeBox.Clear();
+                CellTextBox.Clear();
+                maskedTextBox1.Clear();
+                NameBox.Clear();
+
+                payuButton.Checked = false;
+                ChatterButton.Checked = false;
+                DDButton.Checked = false;
+                MixMaxButton.Checked = false;
+                TandCBox.Checked = false;
+
+                OKbutton.Enabled = false;
+
+                TalkBox.Text = "0";
+                DataBox.Text = "0";
+                AirtimeBox.Text = "0";
+                CellTextBox.Text = "0";
+
+                TalkBox.Enabled = true;
+                DataBox.Enabled = true;
+                AirtimeBox.Enabled = true;
+                CellTextBox.Enabled = true;
+
+
+            }
+        }
+
+        private void EXITbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            if((NameBox.Text==""))
+            {
+                NameBox.BackColor = Color.Red;
+            }
+            if(maskedTextBox1.Text == "")
+            {
+                maskedTextBox1.BackColor = Color.Red;
+            }
+            //if(CellTextBox.Text == "")
+            //{
+            //    CellTextBox.BackColor = Color.Red;
+            //}
+
+            try
+            {
+                airtime = decimal.Parse(AirtimeBox.Text);
+                data = decimal.Parse(DataBox.Text) * 20;
+                talktime = decimal.Parse(TalkBox.Text) * 0.5m;
+            }
+            catch
+            {
+                MessageBox.Show("Enter the correct input");
+            }
+
+            totalcharge = servicecharge + airtime + data + talktime;
+
+            MessageBox.Show("Total charge is"+totalcharge.ToString("n2"));
+            MessageBox.Show("Airtime: R" + AirtimeBox.Text + Environment.NewLine + "Data: " + DataBox.Text + " GB" + Environment.NewLine + "TalkTime: " + TalkBox.Text + " minutes");
+        }
+    }
+
+}
+
+
